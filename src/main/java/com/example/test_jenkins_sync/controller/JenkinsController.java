a5bc38eb93c1aca210205c2af2c1feda9b48d6ad
GiangNXT <Giang.nxt.it@gmail.com>
2023-12-06 15:50:50 +0700

    no message

--- src/main/java/com/example/test_jenkins_sync/controller/JenkinsController.java
+++ src/main/java/com/example/test_jenkins_sync/controller/JenkinsController.java
@@ -1,13 +1,15 @@
 package com.example.test_jenkins_sync.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class JenkinsController {
-    @PostMapping("/jenkins-notification")
-    public void handleJenkinsNotification(@RequestBody String notification) {
-        System.out.println(notification);
+    @GetMapping("/jenkins-notification")
+    public ResponseEntity<?> handleJenkinsNotification(@RequestBody String notification) {
+        return ResponseEntity.ok().body(notification);
     }
 }
